d6d7cf833dc261658cc48959cfbe9eb019521e08
Aur3lus Dev <aur3lius-dev@users.noreply.github.com>
2017-09-23 12:31:46 -0400

    Cleaned up old comments and restructured the way handle_path_vars works

--- plugins/FlaskBottleRoutes.py
+++ plugins/FlaskBottleRoutes.py
@@ -4,7 +4,8 @@ import re
 from random import randint
 
 # Define on a per app basis: dictionary of {var_name: str(type)}
-PATH_VARS = {'uid': 'int', 'aid': 'int', 'pid': 'int'}
+#PATH_VARS = {'unknown': 'int'}
+PATH_VARS = {'<username>': 'str'}
 
 
 def handle_path_vars(var_names):
@@ -14,16 +15,18 @@ def handle_path_vars(var_names):
     """
     ret_val = {}
     for var in var_names:
-        var = var.strip("<").strip(">")
         if var in PATH_VARS.keys():
             if PATH_VARS[var] == "int":
                 ret_val[var] = randint(0, 47)
+            elif PATH_VARS[var] == 'str':
+                ret_val[var] = "test"
             # Define more based on need
             else:
                 ret_val[var] = ""
     return ret_val
 
 def get_ext():
+    """Defines the extension type expected within SpyDir"""
     return ".py"
 
 def run(filename):
@@ -31,20 +34,18 @@ def run(filename):
     SpyDir Extension method contains main function to
         process Flask/Bottle Routes
     """
-    route_rule = """^@*(app\.route\(|^bottle\.route\(|route\()"""
-    path_rule = "(<\w+>)"
+    route_rule = r"""^@*(app\.route\(|^bottle\.route\(|route\()"""
+    path_rule = r"(<\w+>)"
     route_list = []
 
     for line in filename:
         line = line.replace("'", '"').replace('"', "").strip()
         if re.search(route_rule, line):
-            # print(line)
             if "methods" in line:  # this is ignored currently
                 methods = line.split("[")[1].split("]")[0].split(",")
             line = re.split(route_rule, line)[2].split(")")[0].split(',')[0]
             if re.search(path_rule, line):
                 path_values = handle_path_vars(re.findall(path_rule, line))
-                line = line.replace("<", "").replace(">", "")
                 for k, v in path_values.items():
                     line = line.replace(k, str(v))
             route_list.append(line)
